6d13b7dbb99c03ce5ea4097b115255c55d00480b
Nickchen Nick <nickchen120235@gmail.com>
2020-11-16 02:08:26 +0800

    data ready

---
+++ src/utils/series.tsx
@@ -0,0 +1,21 @@
+const series: { [code: string]: string } = {
+  NY_GDP_MKTP_CD: 'GDP (current US$)',
+  NY_GDP_MKTP_KD_ZG: 'GDP growth (annual %)',
+  IT_NET_USER_P2: 'Internet users (per 100 people)',
+  SP_RUR_TOTL: 'Rural population',
+  SP_POP_TOTL_FE_ZS: 'Population, female (% of total)',
+  SP_POP_TOTL: 'Population, total',
+  SP_POP_GROW: 'Population growth (annual %)',
+  NY_GDP_PCAP_KD_ZG: 'GDP per capita growth (annual %)',
+  NY_GDP_PCAP_CD: 'GDP per capita (current US$)',
+  SL_TLF_TOTL_IN: 'Labor force, total',
+  SL_TLF_TOTL_FE_ZS: 'Labor force, female (% of total labor force)',
+  MS_MIL_TOTL_TF_ZS: 'Armed forces personnel (% of total labor force)',
+  AG_SRF_TOTL_K2: 'Surface area (sq. km)',
+  EN_POP_DNST: 'Population density (people per sq. km of land area)',
+  AG_LND_ARBL_ZS: 'Arable land (% of land area)',
+  SL_UEM_TOTL_ZS: 'Unemployment, total (% of total labor force) (modeled ILO estimate)',
+  SL_UEM_TOTL_FE_ZS: 'Unemployment, female (% of female labor force) (modeled ILO estimate)',
+}
+
+export default series
