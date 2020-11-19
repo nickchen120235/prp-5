cda52a555a5bddd07c31bc412791552413cb1444
Nickchen Nick <nickchen120235@gmail.com>
2020-11-19 09:19:42 +0800

    y axis zoomable and color palette

--- src/components/MultiCountryChart.tsx
+++ src/components/MultiCountryChart.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { VictoryChart, VictoryScatter, VictoryTheme } from 'victory'
+import { VictoryChart, VictoryScatter, VictoryTheme, VictoryZoomContainer } from 'victory'
 
 import data from '../utils/data'
+import { colorArray } from '../utils/utils'
 
 interface MultiCountryChartProps {
   series: string
@@ -13,14 +14,14 @@ const MultiCountryChart = (props: MultiCountryChartProps) => {
 
   const renderData = country.map(countryKey => (
     data[countryKey][series].map((value, index) => ({
-      x: index+1990,
-      y: (series === 'SP_RUR_TOTL' || series === 'SL_TLF_TOTL_IN')? value/data[countryKey]['SP_POP_TOTL'][index]*100: value
+      x: index + 1990,
+      y: (series === 'SP_RUR_TOTL' || series === 'SL_TLF_TOTL_IN') ? value / data[countryKey]['SP_POP_TOTL'][index] * 100 : value
     })).filter(entry => !isNaN(entry.y))
   )).filter(entry => entry.length !== 0)
 
   return (
-    <VictoryChart theme={VictoryTheme.material}>
-      {renderData.map(data => <VictoryScatter key={data[0].x} data={data} />)}
+    <VictoryChart theme={VictoryTheme.material} containerComponent={<VictoryZoomContainer zoomDimension='y' />}>
+      {renderData.map((data, index) => <VictoryScatter style={{ data: { fill: colorArray[index % 12] } }} key={data[0].x} data={data} />)}
     </VictoryChart>
   )
 }
--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -3,3 +3,5 @@ export const findKey = (value: string, object: Record<string, unknown>) => {
   if (key === undefined) key = ''
   return key
 }
+
+export const colorArray = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928']
